b3c4c421defc495b08e08448abdb085d27792c1c
Laura Lovato <103641723+lauralovatoc@users.noreply.github.com>
2024-02-07 09:26:48 -0300

    Add files via upload

--- capitalize.py
+++ capitalize.py
@@ -1,5 +1,6 @@
-#receber uma string e deixar a primeira letra de cada palavra em letra maisucula --> funcoes capitalize() e split()
 if __name__ == '__main__':
     s = input()
     result = " ".join(i.capitalize() for i in s.split(" "))
     print(result)
+
+    #recebe string, transofrma primeira letra de cada palavra em maiuscula, funções: capitalize(), split() e join()
