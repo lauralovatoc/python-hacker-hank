1be40b5c0bc2392cbe7da931953f781d6d90d3df
Laura Lovato Camponogara <103641723+lauralovatoc@users.noreply.github.com>
2025-02-20 23:26:24 -0300

    Delete Operadores.py

--- Operadores.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    A = 5
-    B = 15
-    C = 20
-
-    print("A == B and B > C: ", A == B and B > C)
-    print("A < B or B > C: ", A < C or B > C)
-    print("not A == B: ", not A == B)
